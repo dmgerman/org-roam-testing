804a5537885d30bff12bd7f882ab305c841c0e41
Daniel M German <dmg@turingmachine.org>
2025-05-20 07:56:08 -0700

    added some instructions

--- init.el
+++ init.el
@@ -118,3 +118,15 @@ This function is equivalent to the following template
 (global-set-key (kbd "C-c n r") 'consult-org-roam-search)
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
